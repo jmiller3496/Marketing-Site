3dda783b468fd8a135dd31d02992d63cdc5ace4d
jmiller3496 <network@jonathan-m-miller.solutions>
2019-01-16 12:48:58 -0500

    Sync master to Styling

--- src/scheduler/SchedulerPage.jsx
+++ src/scheduler/SchedulerPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import HeaderBar from './../master/HeaderBar'
 import DaySchedule from './DaySchedule';
 import { PanelGroup, Row, Col, Grid, Modal, Button } from 'react-bootstrap';
@@ -97,40 +96,5 @@ class SchedulerPage extends Component {
     );
   }
 }
-=======
-import HeaderBar from './../master/Header'
-import { Panel, PanelGroup, Button } from 'react-bootstrap'
-import './../../node_modules/bootstrap/dist/css/bootstrap.css' 
-
-class SchedulerPage extends Component {
-    constructor(props, context) {
-        super(props, context);
-    
-        this.state = {
-          open: true
-        };
-      }
-    
-      render() {
-        return (
-          <div>
-            <HeaderBar title="Scheduler" />
-            <PanelGroup className="week-group">
-              <Panel className="Monday" defaultExpanded={false}>
-                <Panel.Heading>
-                  <Panel.Title toggle>Monday</Panel.Title>
-                </Panel.Heading>
-                <Panel.Body collapsible>
-                  <div>
-                    --PLACEHOLDER--
-                  </div>
-                </Panel.Body>
-              </Panel>
-            </PanelGroup>
-          </div>
-        );
-      }
-    }
->>>>>>> master
 
 export default SchedulerPage;
