a6eb48eb6913f5ede2b44a35da4b4fbb8b6c982f
jmiller3496 <network@jonathan-m-miller.solutions>
2019-01-30 14:19:39 -0500

    Fix styling and routing for new pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import AboutPage from "./about/AboutPage";
 import HomePage from "./home/HomePage";
 import SoftwarePage from "./software/SoftwarePage";
+import ConstructionPage from "./construction/UnderConstructionPage";
+import ContactPage from "./contact/ContactPage";
+import GiveBackPage from "./give/GiveBackPage";
+import WritingHomePage from "./writing/WritingHomePage";
 
 class Routes extends Component {
     render() {
         return (
             <div>
-                <Route path="/about" component={AboutPage} />
+                <Route path="/give" component={ConstructionPage} />
+                <Route path="/about" component={HomePage} />
+                <Route path="/contact" component={ContactPage} />
                 <Route path="/home" component={HomePage} />
                 <Route path="/software" component={SoftwarePage} />
+                <Route path="/hardware" component={ConstructionPage} />
+                <Route path="/writing" component={WritingHomePage} />
                 <Route path="/" exact component={HomePage} />
             </div>
         )
