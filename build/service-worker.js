7226634b9e2490b0c1daa340a4ede3f7d7a7fedb
jmiller3496 <network@jonathan-m-miller.solutions>
2019-02-15 11:11:44 -0500

    Add missing deployment files to enable functional deploy with Express JS server

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed7feaa3d1e366d97c28a18377d053aa.js"
+  "/precache-manifest.8b0f8c4fb6ec4ea6eea7dfee11395fec.js"
 );
 
 workbox.clientsClaim();
