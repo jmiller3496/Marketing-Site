d1f71341a2388e59505de39209aa91fd3991b825
Jon M Miller <mail@jm-miller.com>
2020-03-08 00:59:02 -0500

    Converts routes component to functional syntax

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import SkillsPage from "./skills/SkillsPage";
 import ContactPage from "./contact/ContactPage";
 import ProjectsPage from "./projects/ProjectsPage";
 
-class Routes extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/home" component={HomePage} />
-                <Route path="/about" component={HomePage} />
-                <Route path="/contact" component={ContactPage} />
-                <Route path="/skills" component={SkillsPage} />
-                <Route path="/projects" component={ProjectsPage} />
-                <Route path="/" exact component={HomePage} />
-            </div>
-        )
-    }
+function Routes() {
+    return (
+        <div>
+            <Route path="/home" component={HomePage} />
+            <Route path="/about" component={HomePage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/skills" component={SkillsPage} />
+            <Route path="/projects" component={ProjectsPage} />
+            <Route path="/" exact component={HomePage} />
+        </div>
+    );
 }
 
 export default Routes;
